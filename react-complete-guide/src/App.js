2454465701b762b6b81e8c9a05c27e499813b11c
Kurt Peniket <kurt.peniket@gmail.com>
2021-03-26 16:05:25 +0000

    new

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -2,22 +2,6 @@ import React, { Component } from 'react';
 import './App.css';
 import Person from './Person/Person';
 import './Person/Person.css';
-import styled from 'styled-components';
-
-const StyledButton = styled.button`
-  background-color: ${props => props.alt ? 'red' : 'green'};
-  color: white;
-  font: inherit;
-  border: 1px solid blue;
-  padding: 8px;
-  cursor: pointer;
-  border-radius: 8px;
-
-  &:hover {
-    background-color: lightgreen;
-    color: black;
-  }
-`;
 
 class App extends Component {
   state = {
@@ -93,10 +77,9 @@ class App extends Component {
       <div className="App">
           <h1>Hi I'm a react app</h1>
           <p className={classes.join(' ')}>This is a paragraph</p>
-          <StyledButton
-            alt={this.state.showPersons}
+          <button
             onClick={this.togglePersonsHandler}> Toggle
-          </StyledButton>
+          </button>
           {persons}
       </div>
     );
