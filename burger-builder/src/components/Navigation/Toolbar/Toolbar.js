d72236d003b5139536adb2ba5a349afcb89cdfa3
Kurt Peniket <kurt.peniket@gmail.com>
2021-04-07 16:54:55 +0100

    fixed toolbar and added responsiveness

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
-import classes from './Toolbar.css';
 
-const toolbar = (props) => (
-  <header className={classes.Toolbar}>
-    <div>Menu</div>
-    <div>Logo</div>
-    <nav>
-      ...
-    </nav>
-  </header>
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggle from '../SideDrawer/DrawerToggle/DrawerToggle';
+
+const toolbar = ( props ) => (
+    <header className={classes.Toolbar}>
+        <DrawerToggle clicked={props.drawerToggleClicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
+            <NavigationItems />
+        </nav>
+    </header>
 );
 
 export default toolbar;
